91f8b64c600fa18807e258ef25eda097e398cbd7
Javier Saiz <javiersaizcano@gmail.com>
2022-11-02 09:51:16 +0100

    setup

---
+++ src/BinarySearch/index.js
@@ -0,0 +1,18 @@
+export default function binarySearch(array, seek) {
+    let hi = array.length;
+    let low = 0;
+
+    do {
+        const m = Math.floor(low + (hi - low) / 2);
+        const v = array[m];
+
+        if (v === seek) return true;
+        else if (v > seek) {
+            hi = m;
+        } else {
+            low = m + 1;
+        }
+    } while (low < hi);
+
+    return false;
+}
